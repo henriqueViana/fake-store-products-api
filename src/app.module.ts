697377467b058d35827eb7fe1e7500fda66c5c18
Henrique Viana <henriquehvisa@gmail.com>
2025-04-23 17:06:41 -0300

    chore: basic config to work with graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/application/http/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      debug: true,
+      playground: true
+    }),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
