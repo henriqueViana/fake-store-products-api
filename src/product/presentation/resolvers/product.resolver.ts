f5c59e03c15f6d7389e02b512c184cabaa7ccc85
Henrique Viana <henriquehvisa@gmail.com>
2025-06-06 11:03:28 -0300

    feat: create query of get-all in product-resolver

--- src/product/presentation/resolvers/product.resolver.ts
+++ src/product/presentation/resolvers/product.resolver.ts
@@ -4,6 +4,7 @@ import { Product } from "../../../product/domain/entities/product.entity";
 import { CreateProductInput } from "../../domain/dto/create-product.input";
 import { CreateProductCommand } from "../../application/commands/create-product/create-product.command";
 import { GetProductQuery } from "../../application/queries/get-product-by-sku/get-product-by-sku.query";
+import { GetAllProductsQuery } from "../../application/queries/get-all-products/get-all-products.query";
 
 @Resolver('Product')
 export class ProductResolver {
@@ -15,6 +16,11 @@ export class ProductResolver {
   @Query(() => Product)
   getProductBySku(@Args('sku') sku: string): Promise<Product> {
     return this.queryBus.execute(new GetProductQuery(sku))
+  }
+
+  @Query(() => [Product])
+  getProducts(): Promise<Product[]> {
+    return this.queryBus.execute(new GetAllProductsQuery())
   }
 
   @Mutation(() => Product)
